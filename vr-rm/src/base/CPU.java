f86cba9c2315b69f52703a9e00606b178d66d4b1
Ernestas <ern.velk@gmail.com>
2021-02-24 21:57:59 +0200

    adding memory sizes and registers

--- vr-rm/src/base/CPU.java
+++ vr-rm/src/base/CPU.java
@@ -1,4 +1,10 @@
 package base;
 
 public class CPU {
+
+    private final int SUPERVISOR = 0;
+    private final int USER = 1;
+
+    private int MODE = 1; //(0 - supervizorius, 1 - vartotojas)
+
 }
--- vr-rm/src/base/Memory.java
+++ vr-rm/src/base/Memory.java
@@ -1,4 +1,9 @@
 package base;
 
 public class Memory {
+    private Word memory[];
+
+    private final int BLOCKSIZE = 16; // words
+    private final int userMemorySize = BLOCKSIZE * 64;
+    private final int externalMemorySize = BLOCKSIZE * 64;
 }
