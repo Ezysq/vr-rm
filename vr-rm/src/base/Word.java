45b20ced192bc25bc041e179e8f361117b31a398
Ernestas <ern.velk@gmail.com>
2021-02-24 21:24:14 +0200

    Filling in Word.java

--- vr-rm/src/base/Word.java
+++ vr-rm/src/base/Word.java
@@ -1,4 +1,26 @@
 package base;
 
 public class Word {
+    public static final int SIZE = 4;
+    private final byte[] data;
+
+    public Word() {
+        this.data = new byte[SIZE];
+    }
+
+    public Word(byte[] data) {
+        this.data = data;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public byte getByte(int i) {
+        return data[i];
+    }
+
+    public void setByte(int i, byte dat) {
+        data[i] = dat;
+    }
 }
