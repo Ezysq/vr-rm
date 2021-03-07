43143f16afd5336fe30183e9b457b129c831ef57
Sonata <sonatapr1@gmail.com>
2021-03-07 14:27:26 +0200

    Added main loop

--- vr-rm/src/base/Main.java
+++ vr-rm/src/base/Main.java
@@ -1,8 +1,23 @@
 package base;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        RM rm = new RM();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true){
+            System.out.println("Command lsit:"); // TODO parasyti kokias komadnas bus galima atlikti
+            try{ // TODO padaryti main loopa kur bus galima ivedinet komandas
+                String arg = reader.readLine();
+                System.out.println(arg);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
